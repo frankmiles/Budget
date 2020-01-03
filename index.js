8d322dbf156a5a21d37b9ce8e1c89b0c89300833
FrankMiles <38081214+frankmiles@users.noreply.github.com>
2020-01-03 20:57:31 +0530

    daily commit!

---
+++ index.js
@@ -0,0 +1,13 @@
+function inputFieldHover(){
+  var email = document.getElementById('field_email');
+  var pass = document.getElementById('field_password');
+  email.style["background"] = "#D63030";
+  pass.style["background"] = "#D63030";
+}
+
+function outputFieldHover(){
+  var email = document.getElementById('field_email');
+  var pass = document.getElementById('field_password');
+  email.style["background"] = "#299D29";
+  pass.style["background"] = "#299D29";
+}
